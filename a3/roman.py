1f52f04026666c7ed1c2e304ed179480c0f6c1da
orca1018 <dev@jiraroj.net>
2025-05-08 13:31:03 +0700

    dont, the extra task. I'll write a test for this one, might be helpful to classmates

--- a3/roman.py
+++ a3/roman.py
@@ -5,6 +5,40 @@
 # Use of AI: NO
 # AI usage details: <DETAILS>
 
-### Your code start here ######
-def toRoman(n: int) -> str:
+# not allowed to use loops and other ds, makes the solution a lot less elegant
 
+### Your code start here ######
+def helper_function(digit: int, decimal: str, five: str, higher_decimal: str) -> str:
+    if digit == 9:
+        return decimal + higher_decimal
+    if digit == 4:
+        return decimal + five
+
+    roman = ''
+    if digit >= 5:
+        roman += five
+        digit -= 5
+    roman += decimal * digit
+    return roman
+
+
+def toRoman(n: int) -> str:
+    roman_number = ''
+
+    units = n % 10
+    n //= 10
+    roman_number = helper_function(units, 'I', 'V', 'X') + roman_number
+
+    tens = n % 10
+    n //= 10
+    roman_number = helper_function(tens, 'X', 'L', 'C') + roman_number
+
+    hundreds = n % 10
+    n //= 10
+    roman_number = helper_function(hundreds, 'C', 'D', 'M') + roman_number
+
+    thousands = n % 10
+    n //= 10
+    roman_number = helper_function(thousands, 'M', 'V̅', 'X̅') + roman_number
+
+    return roman_number
