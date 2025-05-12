2e5ee9ba2970721ee9ddc3d560428a312d4b5ced
orca1018 <dev@jiraroj.net>
2025-05-12 19:06:19 +0700

    Sth got changed, but it should works fine

--- a3/nyctime.py
+++ a3/nyctime.py
@@ -1,7 +1,7 @@
 # Assignment 3, Task 6
 # Name: Jiraroj Wiruchpongsanon
 # Collaborators: <NAME_1, NAME_2>
-# Time Spent: 0.1 hrs
+# Time Spent: 0.2 hrs
 # Use of AI: NO
 # AI usage details: <DETAILS>
 
@@ -12,8 +12,16 @@ def nycHour(bkkHour: int) -> str:
         nyc_hour += 24
 
     prefix = 'am'
-    if nyc_hour > 12:
+    # BKK 11.00 -> NYC 00.00 -> NYC 12am
+    if nyc_hour == 0:
+        nyc_hour = 12
+        prefix = 'am'
+    elif nyc_hour < 12:
+        prefix = 'am'
+    elif nyc_hour == 12:
         prefix = 'pm'
+    else:
         nyc_hour -= 12
+        prefix = 'pm'
 
     return str(nyc_hour) + prefix
